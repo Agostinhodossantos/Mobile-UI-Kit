600e488c70d98df839a7ba2b95cec6e5637d6d11
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-12-14 10:22:38 +0200

    add GetCoinUseCase

---
+++ Crypto/app/src/main/java/com/challenge/crypto/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -0,0 +1,29 @@
+package com.challenge.crypto.domain.use_case.get_coin
+
+import com.challenge.crypto.common.Resource
+import com.challenge.crypto.data.remote.dto.CoinDetail
+import com.challenge.crypto.data.remote.dto.toCoin
+import com.challenge.crypto.data.remote.dto.toCoinDetail
+import com.challenge.crypto.data.repository.CoinRepository
+import com.challenge.crypto.domain.model.Coin
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coin = repository.getCoinById(coinId).toCoinDetail()
+            emit(Resource.Success(coin))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured"))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't reach server. Check your internet connection"))
+        }
+    }
+}
