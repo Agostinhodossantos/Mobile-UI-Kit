75a9a1798618707cbe7563e4d5934f91c448da1a
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-12-14 10:18:16 +0200

    create GetCoinsUseCase

--- Crypto/app/src/main/java/com/challenge/crypto/data/repository/CoinRepositoryImpl.kt
+++ Crypto/app/src/main/java/com/challenge/crypto/data/repository/CoinRepositoryImpl.kt
@@ -1,4 +1,18 @@
 package com.challenge.crypto.data.repository
 
-class CoinRepositoryImpl {
+import com.challenge.crypto.data.remote.CoinPaprikaApi
+import com.challenge.crypto.data.remote.dto.CoinDetailDto
+import com.challenge.crypto.data.remote.dto.CoinDto
+import javax.inject.Inject
+
+class CoinRepositoryImpl @Inject constructor(
+    private val api: CoinPaprikaApi
+): CoinRepository {
+    override suspend fun getCoins(): List<CoinDto> {
+        return api.getCoins()
+    }
+
+    override suspend fun getCoinById(coinId: String): CoinDetailDto {
+        return api.getCoinById(coinId)
+    }
 }
---
+++ Crypto/app/src/main/java/com/challenge/crypto/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -0,0 +1,27 @@
+package com.challenge.crypto.domain.use_case.get_coins
+
+import com.challenge.crypto.common.Resource
+import com.challenge.crypto.data.remote.dto.toCoin
+import com.challenge.crypto.data.repository.CoinRepository
+import com.challenge.crypto.domain.model.Coin
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinsUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coins = repository.getCoins().map { it.toCoin() }
+            emit(Resource.Success(coins))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured"))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't reach server. Check your internet connection"))
+        }
+    }
+}
