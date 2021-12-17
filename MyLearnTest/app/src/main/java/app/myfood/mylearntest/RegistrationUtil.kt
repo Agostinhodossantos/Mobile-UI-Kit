b686b9f6a4b06ee2f028c0b54ce3dcc00707035e
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-12-17 16:55:43 +0200

    implements validate registration input

--- MyLearnTest/app/src/main/java/app/myfood/mylearntest/RegistrationUtil.kt
+++ MyLearnTest/app/src/main/java/app/myfood/mylearntest/RegistrationUtil.kt
@@ -1,4 +1,20 @@
 package app.myfood.mylearntest
 
 object RegistrationUtil {
+
+    private val existingUsers = listOf("Agostinho", "Santos")
+    /**
+     * the input is not valid if...
+     * ...the username/password is empty
+     * ...the username is already token
+     * ...the confirmed password is not the same as the real password
+     * ...the password contains less than 2 digits
+     */
+    fun validateRegistrationInput(
+        username: String,
+        password: String,
+        confirmedPassword: String,
+    ): Boolean {
+        return true
+    }
 }
