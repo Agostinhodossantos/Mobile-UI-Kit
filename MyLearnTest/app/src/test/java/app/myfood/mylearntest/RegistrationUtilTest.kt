a9630bbbe9c5f1bf80a6c6c911cb66530fd2da93
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-12-18 09:06:52 +0200

    implement test methods

--- MyLearnTest/app/src/test/java/app/myfood/mylearntest/RegistrationUtilTest.kt
+++ MyLearnTest/app/src/test/java/app/myfood/mylearntest/RegistrationUtilTest.kt
@@ -14,4 +14,58 @@ class RegistrationUtilTest{
         )
         assertThat(result).isFalse()
     }
+
+    @Test
+    fun `valid username and correctly repeated password returns true`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Helio",
+            "123",
+            "123"
+        )
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `username already existy returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Agostinho",
+            "123",
+            "123"
+        )
+        assertThat(result).isFalse()
+    }
+
+    /**
+     * Password test case
+     */
+    @Test
+    fun `empty password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Agostinho",
+            "",
+            ""
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `password was repeated incorrectly return false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Helio",
+            "123",
+            "1234"
+        )
+        assertThat(result).isFalse()
+    }
+    @Test
+    fun `less than 2 digit password return false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Helio",
+            "12",
+            "12"
+        )
+        assertThat(result).isFalse()
+    }
+
+
 }
