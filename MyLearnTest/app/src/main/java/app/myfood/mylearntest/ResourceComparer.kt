1f088ecfe6f17caf5ea266f6966fc42807c6377e
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-12-21 15:17:03 +0200

    create calc with kotlin

--- MyLearnTest/app/src/main/java/app/myfood/mylearntest/ResourceComparer.kt
+++ MyLearnTest/app/src/main/java/app/myfood/mylearntest/ResourceComparer.kt
@@ -1,4 +1,10 @@
 package app.myfood.mylearntest
 
+
+import android.content.Context
+
 class ResourceComparer {
+    fun isEqual(context: Context, resId: Int, string: String): Boolean {
+        return context.getString(resId) == string
+    }
 }
