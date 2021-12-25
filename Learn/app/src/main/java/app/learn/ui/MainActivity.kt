87065ed8625cbbdc2f2575fc208763f16af2665a
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-12-25 12:40:14 +0200

    create layout fragment

--- Learn/app/src/main/java/app/learn/ui/MainActivity.kt
+++ Learn/app/src/main/java/app/learn/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package app.learn
+package app.learn.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.learn.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
