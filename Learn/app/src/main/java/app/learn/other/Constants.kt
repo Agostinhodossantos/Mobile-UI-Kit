a0d2450bcc9f072ede51ec21e74dae02f1245214
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-12-25 13:51:54 +0200

    implement providesDefaultShoppingRepository

--- Learn/app/src/main/java/app/learn/other/Constants.kt
+++ Learn/app/src/main/java/app/learn/other/Constants.kt
@@ -3,4 +3,7 @@ package app.learn.other
 object Constants {
     const val DATABASE_NAME = "shopping_db"
     const val BASE_URL = "https://pixabay.com/api"
+
+    const val MAX_NAME_LENGTH = 20
+    const val MAX_PRICE_LENGTH = 10
 }
